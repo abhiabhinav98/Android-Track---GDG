e2f135f88799735fefd0b0b4f42e6f7267b081e5
Abhinav Gupta <abhiabhinav98@gmail.com>
2020-02-16 12:06:53 +0530

    x

---
+++ Session 2/GDGFeedbackForm/app/src/main/java/com/gdg/bhopal/gdgfeedbackform/item_layout.java
@@ -0,0 +1,14 @@
+package com.gdg.bhopal.gdgfeedbackform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class item_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_layout);
+    }
+}
